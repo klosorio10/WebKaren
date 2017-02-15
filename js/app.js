2e31125e8e4e2eb6409e8ed6867df74cf91be0ad
klosorio10 <kl.osorio10@uniandes.edu.co>
2017-02-14 22:57:20 -0500

    Mejora de la pagina

---
+++ js/app.js
@@ -0,0 +1,51 @@
+(function(){
+	var app = angular.module('principal',[]);
+	app.controller('principalController', function(){
+		this.projects = proyectos;
+	});
+
+	var proyectos = [
+	{
+		name: 'Nia',
+		date: '10/10/16',
+		description: '',
+		image: "",
+	},
+	{
+		name: 'Nia',
+		date: '10/10/16',
+		description: '',
+		image: "",	
+	},
+	{
+		name: 'Nia',
+		date: '10/10/16',
+		description: '',
+		image: "",	
+	},
+	{
+		name: 'Nia',
+		date: '10/10/16',
+		description: '',
+		image: "",	
+	},
+	{
+		name: 'Nia',
+		date: '10/10/16',
+		description: '',
+		image: "",	
+	},
+	{
+		name: 'Nia',
+		date: '10/10/16',
+		description: '',
+		image: "",	
+	},
+	{
+		name: 'Nia',
+		date: '10/10/16',
+		description: '',
+		image: "",	
+	}];
+
+})();
